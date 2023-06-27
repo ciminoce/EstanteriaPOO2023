0dde558af1499289c1bb63cd0431d14e43501f55
ciminoce <ciminoce@yahoo.com.ar>
2023-06-27 10:22:11 -0300

    Agregar archivos de proyecto.

---
+++ EstanteriaPOO2023.Entidades/Estanteria.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+
+namespace EstanteriaPOO2023.Entidades
+{
+    public class Estanteria
+    {
+        private int ubicacionEstante;
+        private Producto[] productos;
+        private Estanteria(int capacidad)
+        {
+            productos = new Producto[capacidad];
+        }
+        public Estanteria(int capacidad, int ubicacion):this(capacidad)
+        {
+            ubicacionEstante = ubicacion;
+        }
+        public Producto[] GetProductos() { return  productos; }
+
+        public static string MostrarEstanteria(Estanteria e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Estante ubicacion: {e.ubicacionEstante} \n");
+
+            for (int i = 0; i < e.productos.Length; i++)
+            {
+                if (!Object.ReferenceEquals(e.productos[i], null))
+                {
+                    sb.AppendLine(Producto.MostrarProducto(e.productos[i]));
+                    sb.AppendLine("----------------------");
+
+                }
+
+            }
+            return sb.ToString();
+        }
+
+        public static bool operator ==(Estanteria e, Producto p)
+        {
+            foreach (var p2 in e.productos)
+            {
+                if (p2==p)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public static bool operator !=(Estanteria e, Producto p) { return !(e == p); }
+        public static bool operator +(Estanteria e, Producto p)
+        {
+            if (e != p)
+            {
+                for (int i = 0; i < e.productos.Length; i++)
+                {
+                    if (e.productos[i] is null)
+                    {
+                        e.productos[i] = p;
+                        return true;
+
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public static Estanteria operator -(Estanteria e, Producto p)
+        {
+            for (int i = 0; i < e.productos.Length; i++)
+            {
+                if (e == p)
+                {
+                    e.productos[i] = null;
+                    break;
+                }
+            }
+            return e;
+        }
+    }
+}
---
+++ EstanteriaPOO2023.Entidades/Producto.cs
@@ -0,0 +1,49 @@
+﻿namespace EstanteriaPoo2023.Entidades
+{
+    public class Producto
+    {
+        private string codigoDeBarras;
+        private string marca;
+        private float precio;
+
+        public Producto(string codigoDeBarras, string marca, float precio)
+        {
+            this.codigoDeBarras = codigoDeBarras;
+            this.marca = marca;
+            this.precio = precio;
+        }
+        public string GetMarca() => marca;
+        public float GetPrecio() => precio;
+        public static string MostrarProducto(Producto p)
+        {
+            return $"{p.codigoDeBarras}-{p.marca}-{p.precio}";
+        }
+        public static explicit operator string(Producto p)
+        {
+            return p.codigoDeBarras;
+        }
+
+        public static bool operator ==(Producto producto1, Producto producto2)
+        {
+            if (!(producto1 is null) && !(producto2 is null))
+            {
+                return producto1.codigoDeBarras == producto2.codigoDeBarras
+                    && producto1.marca.ToUpper() == producto2.marca.ToUpper();
+
+            }
+            return false;
+        }
+        public static bool operator !=(Producto producto1, Producto producto2)
+        {
+            return !(producto1 == producto2);
+        }
+        public static bool operator ==(Producto producto, string marca)
+        {
+            return producto.marca.ToUpper() == marca.ToUpper();
+        }
+        public static bool operator !=(Producto producto, string marca)
+        {
+            return !(producto == marca);
+        }
+    }
+}
